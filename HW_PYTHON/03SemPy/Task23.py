5e64078670ae0c856bd4a5e5d0d5c4f915e3068d
Tatiana Verlova <tatiana.verlova@gmail.com>
2022-10-16 18:07:00 +0300

    add homework to the 3 seminar

--- HW_PYTHON/03SemPy/Task23.py
+++ HW_PYTHON/03SemPy/Task23.py
@@ -3,3 +3,16 @@
 # o [2, 3, 4, 5, 6] => [12, 15, 16];
 # o [2, 3, 5, 6] => [12, 15]
 
+def SumOfPairs(list):
+    result = []
+    for i in range(len(list)//2):
+        item = list[i]*list[(i + 1)*(-1)]
+        result.append(item)
+    if len(list) % 2:
+        item = list[len(list)//2] ** 2
+        result.append(item)
+    return result
+list1 = [2, 3, 4, 5, 6]
+list2 = [2, 3, 5, 6]
+print(SumOfPairs(list1))
+print(SumOfPairs(list2))
--- HW_PYTHON/03SemPy/Task25.py
+++ HW_PYTHON/03SemPy/Task25.py
@@ -4,3 +4,12 @@
 # o 3 -> 11
 # o 2 -> 10
 
+def ScaleOfNotation(num): 
+    if num < 2:
+        return str(num)
+    else:
+        next = str(int( * round(num % 2, 1)))
+        num = int(num // 2)
+        return ScaleOfNotation(num) + next
+number = int(input('Введите число: '))
+print(ScaleOfNotation(number))
