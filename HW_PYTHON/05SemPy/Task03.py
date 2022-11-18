da7561ae1283ee7d9e30e3af765cecc9c20fcbd6
Tatiana Verlova <tatiana.verlova@gmail.com>
2022-11-19 00:09:35 +0300

    add hw to the 5 sem

--- HW_PYTHON/05SemPy/Task03.py
+++ HW_PYTHON/05SemPy/Task03.py
@@ -1,9 +1,7 @@
 # 3. Создайте программу для игры в ""Крестики-нолики"".
+from random import randint
 
-def move(player):
-    print(f"{field['a_1']}   |   {field['a_2']}   |   {field['a_3']}\n_________________\n{field['b_1']}   |   {field['b_2']}   |   {field['b_3']}\n_________________\n{field['c_1']}   |   {field['c_2']}   |   {field['c_3']}")
-    new_move = input(f"Игрок {player}, ваш ход: ")
-    field[new_move] = player
+player = randint(1, 2)    
 
 field = \
     {
@@ -18,6 +16,35 @@ field = \
         'c_3': 'c_3'
     }
 
-print(f"{field['a_1']}   |   {field['a_2']}   |   {field['a_3']}\n_________________\n{field['b_1']}   |   {field['b_2']}   |   {field['b_3']}\n_________________\n{field['c_1']}   |   {field['c_2']}   |   {field['c_3']}")
-
-end_game = 0
+for i in range(0,9):
+    if field['a_1'] == ' o ' and field['a_2'] == ' o ' and field['a_3'] == ' o ' or\
+        field['b_1'] == ' o ' and field['b_2'] == ' o ' and field['b_3'] == ' o ' or\
+        field['c_1'] == ' o ' and field['c_2'] == ' o ' and field['c_3'] == ' o ' or\
+        field['a_1'] == ' o ' and field['b_1'] == ' o ' and field['c_1'] == ' o ' or\
+        field['a_2'] == ' o ' and field['b_2'] == ' o ' and field['c_2'] == ' o ' or\
+        field['a_3'] == ' o ' and field['b_3'] == ' o ' and field['c_3'] == ' o ' or\
+        field['a_1'] == ' o ' and field['b_2'] == ' o ' and field['c_3'] == ' o ' or\
+        field['c_1'] == ' o ' and field['b_2'] == ' o ' and field['a_3'] == ' o ' or\
+        field['a_1'] == ' x ' and field['a_2'] == ' x ' and field['a_3'] == ' x ' or\
+        field['b_1'] == ' x ' and field['b_2'] == ' x ' and field['b_3'] == ' x ' or\
+        field['c_1'] == ' x ' and field['c_2'] == ' x ' and field['c_3'] == ' x ' or\
+        field['a_1'] == ' x ' and field['b_1'] == ' x ' and field['c_1'] == ' x ' or\
+        field['a_2'] == ' x ' and field['b_2'] == ' x ' and field['c_2'] == ' x ' or\
+        field['a_3'] == ' x ' and field['b_3'] == ' x ' and field['c_3'] == ' x ' or\
+        field['a_1'] == ' x ' and field['b_2'] == ' x ' and field['c_3'] == ' x ' or\
+        field['c_1'] == ' x ' and field['b_2'] == ' x ' and field['a_3'] == ' x ':
+        print(f'Игрок {player} победил!')
+        break
+    else:
+        if player == 1:
+            player = 2
+        else:
+            player = 1
+        print(f"{field['a_1']}   |   {field['a_2']}   |   {field['a_3']}\n_______________________\n{field['b_1']}   |   {field['b_2']}   |   {field['b_3']}\n_______________________\n{field['c_1']}   |   {field['c_2']}   |   {field['c_3']}")
+        new_move = input(f"Игрок {player}, ваш ход (введите позицию, например 'a_1'): ")
+        if player == 1:
+            field[new_move] = ' x '
+        else:
+            field[new_move] = ' o '
+else:
+    print('Ничья!')
--- HW_PYTHON/05SemPy/Task04.py
+++ HW_PYTHON/05SemPy/Task04.py
@@ -1,1 +1,25 @@
 # 4. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+
+def compr(string):
+    compression = []
+    i = 0
+    while i < len(string):
+        count = 1
+        while i + 1 < len(string) and string[i] == string[i + 1]:
+            count += 1
+            i += 1
+        compression.append(count) 
+        compression.append(str(string[i]))
+        i += 1
+    return compression
+
+def decompr(compression):
+    decompression = ""
+    for i in range(0, len(compression), 2):
+        decompression += compression[i] * compression[i + 1]
+    return decompression
+
+input_data = input("Введите данные, котрые необходимо сжать: ")
+compressed_data = compr(input_data)
+decompressed_data = decompr(compressed_data)
+print(f"Сжатые данные:\n{compressed_data}\nВосстановленные данные:\n{decompressed_data}")
